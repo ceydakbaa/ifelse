9d84727d6f7de2f8a97c64638aa5e99a070c1346
Ceyda Kaba <ceydakaba@gmail.com>
2025-05-22 21:28:16 +0300

    commit

---
+++ src/ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+
+    public static void main(String[] args) {
+        int number1 = 92 ;
+        int number3 = 83 ;
+          if (number1==number3){
+            System.out.println("number1 ve number3 birbirine eşittir.");
+        } else {
+              System.out.println("number1 ve number3 birbirine eşit değildir.");
+            }
+          }
+    }
+
