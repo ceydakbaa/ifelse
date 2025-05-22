3a6291dd74e18bb390696a1e6f7f08589289496d
Ceyda Kaba <ceydakaba@gmail.com>
2025-05-23 00:16:29 +0300

    commit

---
+++ src/forexample.java
@@ -0,0 +1,8 @@
+public class forexample {
+    public static void main(String[] args) {
+        // 0'dan 100'e kadar olan çift sayıları yazdır
+        for(int i=0; i <=100; i++) {
+            System.out.println(i);
+        }
+    }
+}
