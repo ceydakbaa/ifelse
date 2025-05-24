32af6b6b9586e4febd7cc81ace07e06d1abcd6d3
Ceyda Kaba <ceydakaba@gmail.com>
2025-05-24 10:36:58 +0300

    commit

---
+++ src/ifElsernek2.java
@@ -0,0 +1,11 @@
+class ifElseornek2 {
+    public static void main(String[] args) {
+                int number1 = 92 ;
+                int number3 = 83 ;
+                if (number1==number3){
+                    System.out.println("number1 ve number3 birbirine eşittir.");
+                } else {
+                    System.out.println("number1 ve number3 birbirine eşit değildir.");
+                }
+    }
+}
